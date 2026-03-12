c8905a584609b36d8d86cf268793a0c54fa454b6
Cliff Rosen <cliff.rosen@gmail.com>
2026-03-12 15:44:08 -0600

    backup

--- server/index.js
+++ server/index.js
@@ -18,6 +18,16 @@ const server = http.createServer(app);
 const { broadcast, broadcastGlobal } = createWSServer(server);
 
 // Middleware
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  }
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
 app.use(express.json());
 
 // Static assets (legacy: landing page, display page, CSS, favicon)
